9bd9d437543d788ba0c05bbc55fe4a120501b587
chinnajee <chinnajee@users.noreply.github.com>
2022-08-24 15:50:35 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ public class Main {
     int x = 17;
     int y = 16;
     int sum = x + y;
-    System.out.println("Sum:"sum); // Print the sum of x + y
+    System.out.println("Sum:",sum); // Print the sum of x + y
   }
 }
